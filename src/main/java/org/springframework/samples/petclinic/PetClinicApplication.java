08adc707c964fbd4d5554e6b5ba2c9a02badb762
Carlos Lagos Urbina <clagosu@MCAD72XA4.local>
2022-06-13 19:54:21 -0400

    Se agrega password

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		String password = "1234";
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
